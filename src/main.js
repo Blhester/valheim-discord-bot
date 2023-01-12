73761896f7d36557d0d9fc4b3dabfb59898923be
Benjamin Hester <blhester@liberty.edu>
2023-01-12 00:53:43 -0500

    switched back to exec

--- src/main.js
+++ src/main.js
@@ -30,13 +30,13 @@ client.on('messageCreate', (message) => {
     if(command === 'restart_server') {
         let pidOfServer;
         exec('pidof -s valheim_server.x86_64', (error, stdout, stderr) => {
-            if (error != null)  {
+            if (error !== null)  {
                 sendMessage(message, 'Server is offline, attempting to boot up server');
                 executeStartScript(message); 
             } else if (stdout !== null) {
                 pidOfServer = stdout.toString().trim();
                 exec(`sudo kill -9 ${pidOfServer}`, (error, stdout) => {
-                    if(error != null) {
+                    if(error !== null) {
                         sendMessage(message, 'Error when trying to kill the server');
                         console.log(error.toString());
                     } else {
@@ -50,13 +50,13 @@ client.on('messageCreate', (message) => {
     if(command === 'status') {
         let pidOfServer;
         exec('pidof -s valheim_server.x86_64', (error, stdout, stderr) => {
-            if (error != null)  {
+            if (error !== null)  {
                 sendMessage(message, 'Unable to find the status of the server. The server must be offline');
                 console.log(error.toString()); 
             } else if (stdout !== null) {
                 pidOfServer = stdout.toString().trim();
                 exec(`ps -p ${pidOfServer} -o etime`, (error, stdout) => {
-                    if(error != null) {
+                    if(error !== null) {
                         sendMessage(message, 'Unable to find the status of the server. The server must be offline');
                         console.log(error.toString());
                     } else {
@@ -69,10 +69,14 @@ client.on('messageCreate', (message) => {
 });
 
 function executeStartScript(message) {
-    spawn(`${serverLocation}./start_server.sh`, {
-        stdio: ['ignore', outputFile, 'ignore'],
-        detached: true
-    }).unref();
+    exec(`${serverLocation}./start_server.sh`, (error, stdout, stderr) => {
+        if (error !== null) {
+            sendMessage(message, 'There was an error when trying to start the server');
+            console.log(error.toString());
+        } else if (stdout !== null) {
+
+        }
+    });
 }
 
 function sendMessage(message, messageToSend) {
