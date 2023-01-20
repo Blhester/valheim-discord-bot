c80dc82f30389ef9d0f02871a00bdb3c782c2450
Benjamin Hester <blhester@liberty.edu>
2023-01-20 14:19:11 -0500

    Created a CommandTypes enum to hold the possible commands and moved to using a switch instead of multiple if statements

---
+++ src/assets/CommandType.js
@@ -0,0 +1,9 @@
+export const CommandTypes = {
+    HELP: "help",
+    PING: "ping",
+    ECHO: "echo",
+    NUMBER_OF_PLAYERS: "players",
+    STOP_SERVER: "stop_server",
+    START_SERVER: "start_server",
+    STATUS: "status",
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const exec = require('child_process').exec;
 const execFile = require('child_process').execFile;
+const {PING, ECHO, NUMBER_OF_PLAYERS, STOP_SERVER, START_SERVER, STATUS, HELP} = require('./assets/CommandType');
 
 const commandPrefix = '!';
 const serverLocation = '/home/pi/valheim_server/';
 
 const config = require('config');
+const { CommandTypes } = require('./assets/CommandType');
 const botSecret = config.get('bot.secret');
 const milliSecondsInAnHour = 3600000;
 
@@ -35,114 +37,121 @@ client.on('messageCreate', (message) => {
     console.log(`Got command ${message.content}`);
     const args = message.content.slice(commandPrefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
-    if(command === 'ping') {
-        sendMessage(message, 'pong');
+    const argsWithoutSplit = message.content.slice(commandPrefix.length + command.length);
+    const possibleCommands = () => {
+        var allCommands = Object.values(CommandTypes);
+        var listOfCommands;
+        allCommands.forEach(command => listOfCommands+=`!${command}\n`);
+        return listOfCommands;
     }
 
-    if(command.startsWith('echo')) {
-        const embed = new EmbedBuilder()
-        .setAuthor({
-            name: 'Hello, ' + message.channel.name,
-            iconURL: message.author.avatarURL({ dynamic:true }),
-        })
-        .setTitle('Echo')
-        .setDescription(`${message.author.username} said "${message.content.slice(commandPrefix.length + command.length)}"`)
-        .setColor('Gold');
-
-        sendEmbedToChannel(embed);
-    }
-
-    if(command === 'players') {
-       exec(`cat ${serverLocation}output.log | egrep -o "Connections [0-9]{1,2}" | tail -1 | egrep -o "[0-9]{1,2}"`, (error, stdout) => {
-        if(error !== null) {
-            sendMessage(message, `There was an error trying to find the number of players on the server`);
-            console.log(error.toString());
-        } else if (stdout !== null) {
-            numberOfPlayersOnServer = stdout.toString().trim();
-            sendMessage(message, `There are currently ${numberOfPlayersOnServer} player(s) on the server`);
+    switch(command) {
+        case PING: {
+            sendMessage(message, 'pong');
         }
-       }); 
-    }
 
-    if(command === 'stop_server') {
-        let pidOfServer;
-        exec('pidof -s valheim_server.x86_64', (error, stdout, stderr) => {
-            if (error !== null)  {
-                sendMessage(message, 'Unable to find the status of the server. The server must be offline');
-                console.log(error.toString()); 
-            } else if (stdout !== null) {
-                pidOfServer = stdout.toString().trim();
-                exec(`sudo kill -9 ${pidOfServer}`, (error, stdout) => {
-                    if(error !== null) {
-                        sendMessage(message, 'Error when trying to kill the server');
-                        console.log(error.toString());
-                    } else if (stdout !== null) {
-                        sendMessage(message, 'Server has been shut down');
-                    }
-                }).unref();
-	        }	 
-        }).unref();
-    }
+        case ECHO: {
+            const embed = new EmbedBuilder()
+            .setAuthor({
+                name: 'Hello, ' + message.channel.name,
+                iconURL: message.author.avatarURL({ dynamic:true }),
+            })
+            .setTitle('Echo')
+            .setDescription(`${message.author.username} said "${message.content.slice(commandPrefix.length + command.length)}"`)
+            .setColor('Gold');
+    
+            sendEmbedToChannel(embed);
+        }
 
-    if(command === 'restart_server') {
-        let pidOfServer;
-        exec('pidof -s valheim_server.x86_64', (error, stdout) => {
-            if (error !== null)  {
-                sendMessage(message, 'Server is offline, attempting to boot up server');
-                timeOfLastRestart = new Date(Date.now());
-                executeStartScript(message); 
-            } else if (stdout !== null) {
-                pidOfServer = stdout.toString().trim();
-                exec(`sudo kill -9 ${pidOfServer}`, (error, stdout) => {
-                    if(error !== null) {
-                        sendMessage(message, 'Error when trying to kill the server');
-                        console.log(error.toString());
-                    } else if (stdout !== null) {
-                        timeOfLastRestart = new Date(Date.now());
-                        executeStartScript(message);
-                    }
-                }).unref();
-	        }	 
-        }).unref();
-    }
+        case NUMBER_OF_PLAYERS: {
+            exec(`cat ${serverLocation}output.log | egrep -o "Connections [0-9]{1,2}" | tail -1 | egrep -o "[0-9]{1,2}"`, (error, stdout) => {
+                if(error !== null) {
+                    sendMessage(message, `There was an error trying to find the number of players on the server`);
+                    console.log(error.toString());
+                } else if (stdout !== null) {
+                    numberOfPlayersOnServer = stdout.toString().trim();
+                    sendMessage(message, `There are currently ${numberOfPlayersOnServer} player(s) on the server`);
+                }
+               }); 
+        }
 
-    if(command === 'status') {
-        let pidOfServer;
-        exec('pidof -s valheim_server.x86_64', (error, stdout) => {
-            if (error !== null)  {
-                sendMessage(message, 'Unable to find the status of the server. The server must be offline');
-                console.log(error.toString()); 
-            } else if (stdout !== null) {
-                pidOfServer = stdout.toString().trim();
-                exec(`ps -p ${pidOfServer} -o etime | egrep '[0-9]{1,2}:[0-9]{1,2}'`, (error, stdout) => {
-                    if(error !== null) {
-                        sendMessage(message, 'Unable to find the status of the server. The server must be offline');
-                        console.log(error.toString());
-                    } else if (stdout !== null) {
-                        exec(`cat ${serverLocation}output.log | grep "Load world:" | tail -1`, (error, stdout) => {
-                            if(error !== null) {
-                                console.log(error.toString());                        
-                            } else if (stdout !== null) {
-                                let timeOfServerBootInLogs = new Date(stdout.toString().trim().slice(0, 18));
-                                if (timeOfLastRestart === null) {
-                                    console.log(`Time of lastRestart is null`);
-                                    timeOfLastRestart = timeOfServerBootInLogs;
+        case STOP_SERVER: {
+            let pidOfServer;
+            exec('pidof -s valheim_server.x86_64', (error, stdout) => {
+                if (error !== null)  {
+                    sendMessage(message, 'Unable to find the status of the server. The server must be offline');
+                    console.log(error.toString()); 
+                } else if (stdout !== null) {
+                    pidOfServer = stdout.toString().trim();
+                    exec(`sudo kill -9 ${pidOfServer}`, (error, stdout) => {
+                        if(error !== null) {
+                            sendMessage(message, 'Error when trying to kill the server');
+                            console.log(error.toString());
+                        } else if (stdout !== null) {
+                            sendMessage(message, 'Server has been shut down');
+                        }
+                    }).unref();
+                }	 
+            }).unref();
+        }
+
+        case START_SERVER: {
+            exec('pidof -s valheim_server.x86_64', (error, stdout) => {
+                if (error !== null)  {
+                    sendMessage(message, 'Attempting to bring the server online. Type !status for details');
+                    timeOfLastRestart = new Date(Date.now());
+                    executeStartScript(message); 
+                } else if (stdout !== null) {
+                    sendMessage(message, 'Server is currently online already, please !stop_server first.');
+                }	 
+            }).unref();
+        }
+
+        case STATUS: {
+            let pidOfServer;
+            exec('pidof -s valheim_server.x86_64', (error, stdout) => {
+                if (error !== null)  {
+                    sendMessage(message, 'Unable to find the status of the server. The server must be offline');
+                    console.log(error.toString()); 
+                } else if (stdout !== null) {
+                    pidOfServer = stdout.toString().trim();
+                    exec(`ps -p ${pidOfServer} -o etime | egrep '[0-9]{1,2}:[0-9]{1,2}'`, (error, stdout) => {
+                        if(error !== null) {
+                            sendMessage(message, 'Unable to find the status of the server. The server must be offline');
+                            console.log(error.toString());
+                        } else if (stdout !== null) {
+                            exec(`cat ${serverLocation}output.log | grep "Load world:" | tail -1`, (error, stdout) => {
+                                if(error !== null) {
+                                    console.log(error.toString());                        
+                                } else if (stdout !== null) {
+                                    let timeOfServerBootInLogs = new Date(stdout.toString().trim().slice(0, 18));
+                                    if (timeOfLastRestart === null) {
+                                        console.log(`Time of lastRestart is null`);
+                                        timeOfLastRestart = timeOfServerBootInLogs;
+                                    }
+                                    console.log(`Time of ${timeOfServerBootInLogs}`)
+                                    if (timeOfLastRestart > timeOfServerBootInLogs) {
+                                        sendMessage(message, 'Server is still booting back up');
+                                    } else {
+                                        timeOfLastRestart = timeOfServerBootInLogs;
+                                        sendMessage(message, `Server has been online since ${timeOfServerBootInLogs.toString()}`);	
+                                    }
                                 }
-                                console.log(`Time of ${timeOfServerBootInLogs}`)
-                                if (timeOfLastRestart > timeOfServerBootInLogs) {
-                                    sendMessage(message, 'Server is still booting back up');
-                                } else {
-                                    timeOfLastRestart = timeOfServerBootInLogs;
-                                    sendMessage(message, `Server has been online since ${timeOfServerBootInLogs.toString()}`);	
-                                }
-                            }
-                        }).unref()
-                    }
-                }).unref();
-	        }	 
-        }).unref();
-    };
+                            }).unref()
+                        }
+                    }).unref();
+                }	 
+            }).unref();
+        }
+
+        case HELP: {
+            sendMessage(message, `Here is the list of possible commands:\n${possibleCommands}`)
+        }
+
+        default: {
+            sendMessage(message, `${argsWithoutSplit} is not a valid command. Type !help to get all valid commands`);
+        }
+    }
 });
 
 function executeStartScript(message) {
