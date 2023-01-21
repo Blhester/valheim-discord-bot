f480ded7b4396bd4166c80f89a0fe4228cadeaad
Benjamin Hester <blhester@liberty.edu>
2023-01-20 19:27:30 -0500

    Moved command list to the default.json and cleaned up the execution order of the methods and main.js

---
+++ src/assets/server_controller.js
@@ -0,0 +1,24 @@
+import { exec } from "child_process";
+
+
+export function processIsRunning(serverProcessName) {
+    exec(`pidof -s ${serverProcessName}`, (error, stdout) => {
+        if (error !== null)  {
+            console.log(error.toString());
+        } else if (stdout !== null) {
+            return true;
+        }
+        return false; 
+    });
+}
+
+export function getLastStartTimeOfServerInLogs(outputLogPath) {
+    exec(`cat ${outputLogPath} | grep "Load world:" | tail -1`, (error, stdout) => {
+        if (error !== null) {
+            new Error('Unable to find a startTime in the logs');
+            console.log(error.toString());
+        } else if (stdout !== null) {
+            return new Date(stdout.toString().trim().slice(0, 18));
+        }
+    });
+}
