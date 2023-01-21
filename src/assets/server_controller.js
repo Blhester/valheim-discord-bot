0f0bb44b2a990c6a0b881200dd918d5275569d53
Benjamin Hester <blhester@liberty.edu>
2023-01-20 20:09:37 -0500

    fixed exec function usage

--- src/assets/server_controller.js
+++ src/assets/server_controller.js
@@ -3,26 +3,29 @@ import { exec } from "child_process";
 
 export function processIsRunning(serverProcessName) {
     let isRunning = false;
-    const {stdout} = exec(`pidof -s ${serverProcessName}`, (error) => {
-        if (error !== null) {
-            console.log(error.toString());
-        } 
-    });
+    const {stdout} = exec(`pidof -s ${serverProcessName}`);
+
+    if (error !== null) {
+        console.log(error.toString());
+    }
+
     if (stdout !== null) isRunning = true;
+
     return isRunning;
 }
 
 export function getLastStartTimeOfServerInLogs(outputLogPath) {
     let startTime;
-    exec(`cat ${outputLogPath} | grep "Load world:" | tail -1`, (error, stdout) => {
-        if (error !== null) {
-            new Error('Unable to find a startTime in the logs');
-            console.log(error.toString());
-            return;
-        } else if (stdout !== null) {
-            startTime = new Date(stdout.toString().trim().slice(0, 18));
-            return;
-        }
-    });
+    const {stdout, error} = exec(`cat ${outputLogPath} | grep "Load world:" | tail -1`);
+    
+    if (error !== null) {
+        new Error('Unable to find a startTime in the logs');
+        console.log(error.toString());
+    }
+
+    if (stdout !== null) {
+        startTime = new Date(stdout.toString().trim().slice(0, 18));
+    }
+
     return startTime;
 }
