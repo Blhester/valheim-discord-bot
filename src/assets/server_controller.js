49ec4f3385b532b49223dbc8cf2bcf10bfea1393
Benjamin Hester <blhester@liberty.edu>
2023-01-20 19:46:26 -0500

    used variable outside of exec statement

--- src/assets/server_controller.js
+++ src/assets/server_controller.js
@@ -2,23 +2,26 @@ import { exec } from "child_process";
 
 
 export function processIsRunning(serverProcessName) {
-    return exec(`pidof -s ${serverProcessName}`, (error, stdout) => {
+    let isRunning = false;
+    exec(`pidof -s ${serverProcessName}`, (error, stdout) => {
         if (error !== null)  {
             console.log(error.toString());
         } else if (stdout !== null) {
-            return true;
-        }
-        return false; 
-    }).unref();
+            isRunning = true;
+        } 
+    });
+    return isRunning;
 }
 
 export function getLastStartTimeOfServerInLogs(outputLogPath) {
-    return exec(`cat ${outputLogPath} | grep "Load world:" | tail -1`, (error, stdout) => {
+    let startTime;
+    exec(`cat ${outputLogPath} | grep "Load world:" | tail -1`, (error, stdout) => {
         if (error !== null) {
             new Error('Unable to find a startTime in the logs');
             console.log(error.toString());
         } else if (stdout !== null) {
-            return new Date(stdout.toString().trim().slice(0, 18));
+            startTime = new Date(stdout.toString().trim().slice(0, 18));
         }
-    }).unref();
+    });
+    return startTime;
 }
